d4bd6101bfa27ecee7312dc9d71e280fa281295a
mrisumon <mrisumon121@gmail.com>
2024-02-16 02:43:57 +0600

    creating category using ajax, working process continue

---
+++ resources/views/admin/category/create.blade.php
@@ -0,0 +1,140 @@
+@extends('admin.layouts.app')
+@section('content')
+	<!-- Content Header (Page header) -->
+	<section class="content-header">					
+		<div class="container-fluid my-2">
+			<div class="row mb-2">
+				<div class="col-sm-6">
+					<h1>Create Category</h1>
+				</div>
+				<div class="col-sm-6 text-right">
+					<a href="categories.html" class="btn btn-primary">Back</a>
+				</div>
+			</div>
+		</div>
+		<!-- /.container-fluid -->
+	</section>
+	<!-- Main content -->
+	<section class="content">
+		<!-- Default box -->
+		<div class="container-fluid">
+            <form action="" method="post" id="categoryForm" name="categoryForm">
+                <div class="card">
+                    <div class="card-body">								
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="mb-3">
+                                    <label for="name">Name</label>
+                                    <input type="text" name="name" id="name" class="form-control" placeholder="Name">
+                                    <p></p>	
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="mb-3">
+                                    <label for="slug">Slug</label>
+                                    <input type="text" name="slug" id="slug" class="form-control" placeholder="Slug">
+                                    <p></p>	
+                                </div>
+                            </div>									
+                            <div class="col-md-6">
+                                <div class="mb-3">
+                                    <label for="status">Status</label>
+                                    <select name="status" id="status" class="form-control">
+                                        <option value="1">Active</option>
+                                        <option value="0">Block</option>
+                                    </select>
+                                </div>
+                            </div>									
+                        </div>
+                    </div>							
+                </div>
+                <div class="pb-5 pt-3">
+                    <button type="submit" class="btn btn-primary">Create</button>
+                    <a href="#" class="btn btn-outline-dark ml-3">Cancel</a>
+                </div>
+            </form>
+		</div>
+		<!-- /.card -->
+	</section>
+	<!-- /.content -->
+@endsection
+
+
+<!-- আমরা ajax এর মাধ্যেমে ফর্মের সকল ডাটা ডাটাবেসে পাঠিয়ে দিবো। -->
+<!-- @section('customJs') এর জন্য app.blade.php ফাইলে @yield('customJs') এ্যাড করা আছে। -->
+<!-- নিচের ফরম্যাটটি হলো ajax এর একটি কমন ফরম্যাট, যেটা ডেভেলপাররা ব্যবহার করে ফর্মের ডাটা ডাটাবেসে পাঠানোর জন্য। -->
+@section('customJs')
+    <script>
+        //$(): হলো jQuery এর একটি সিলেক্টর ফাংশন, যার কাজ হলো এক বা একাধিক HTML ইলিমেন্টকে সিলেক্ট করা।
+        //#categoryForm: ফর্মের আইডিকে এখানে ক্যাচ করার হয়েছে, ফর্মকে সিলেক্ট করার জন্য।
+        //submit(): এখানে হলো jQuery এর একটি মেথড, এর কাজ হলো ইভেন্ট হ্যান্ডেলার ফাংশনটিকে, ফর্মের সাবমিট বাটনের সাথে attach করে দেয়া, অর্থাৎ যখন ফর্মের সাবমিট বাটনে ক্লিক করা হবে, তখন এই ইভেন্ট হ্যান্ডেলার ফাংশনটি এক্সিকিউট হবে।
+        $("#categoryForm").submit(function(event){
+            //নরমালি আমরা যখন কোনো ডাটা সাবমিট করি বা কোনো একটি ইভেন্ট ঘটে, তখন সাবমিশন ফর্মের পেজটি রি-লোড হয়, এই ফাংশন পেজকে রি-লোড হতে বাধা দেয়।
+            event.preventDefault();
+            var element = $(this);
+            $.ajax({
+                //url: আমরা ajax এর মাধ্যেমে যে রাউটে ডাটা পাঠাতে চাই, সেই রাউটের url এখানে বলে দিতে হবে।
+                url: '{{route("categories.store")}}',
+                //type: আমরা যে ডাটা পাঠাচ্ছি তা কী টাইপের, যেমন: get, post ইত্যাদি তা বলে দিতে হবে।
+                type:'post',
+                //ফর্মের ডাটা ডাটাবেসে পাঠানোর পূর্বে, ফর্মের ডাটাগুলোকে একটি এ্যারেতে serialize করে, তারপর ডাটাবেসে পাঠায়।
+                data: element.serializeArray(),
+                //সার্ভার থেকে যখন কোনো ডাটা আসবে বা যখন কোনো রেসপন্স আসবে তা কোন ফরম্যাটে আসবে তা তা নির্ধারন করে, আমরা সার্ভারের রেসপন্স ডাটা json ফরম্যাটে দেখতে চাই।
+                dataType: 'json',
+                //success: function(response): যদি ডাটাবেসে ডাটা সাকসেসফুলি যায়, তাহলে এই ফাংশনটি কল হবে।
+                success: function(response){
+
+                    if(response["status"]==true){
+                        
+                    }else{
+                        //response['errors']; সার্ভারে ডাটা যাওয়ার পর সার্ভার রেসপন্স যদি এ্যারর আসে, তাহলে তা errors ভ্যারিয়েবলে এ্যাসাইন হবে।
+                        var errors = response['errors'];
+                        //যদি এ্যারর অবজেক্ট এর প্রপার্টি name হয়, তাহলে কন্ডিশনে প্রবেশ করবে।
+                        if(errors['name']){
+                            //name input tag এ সিএসএস এর একটি ক্লাস is-invalid এ্যাড করে দিবে।
+                            $("#name").addClass('is-invalid')
+                            //p tag কে name input tag এর siblings বানাবে, এবং এখানে এ্যারর ম্যাসেজটি দেখাবে।
+                            .siblings('p')
+                            //p tag এ সিএসএস এর একটি ক্লাস invalid-feedback এ্যাড করে দিবে।
+                            .addClass('invalid-feedback')
+                            //html পেজে name ট্যাগের যে এ্যারর গুলো আসবে, তাদের একটি এ্যারে ক্রিয়েট করবে।
+                            .html(errors['name']);
+                        }else{
+                            $("#name").removeClass('is-invalid')
+                            .siblings('p')
+                            .removeClass('invalid-feedback')
+                            .html("");
+                        }
+
+                        if(errors['slug']){
+                            $("#slug").addClass('is-invalid')
+                            .siblings('p')
+                            .addClass('invalid-feedback')
+                            .html(errors['slug']);
+                        }else{
+                            $("#slug").removeClass('is-invalid')
+                            .siblings('p')
+                            .removeClass('invalid-feedback')
+                            .html("");
+                        }
+                    }
+
+                }, error:function(jqXHR,exception){
+                    console.log("Something went wrong");
+                }
+            })
+        });
+    </script>
+@endsection
+
+
+
+
+
+
+
+
+
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AdminLoginController;
+use App\Http\Controllers\admin\CategoryController;
 use App\Http\Controllers\admin\HomeController;
 
 Route::get('/', function () {return view('welcome');});
@@ -14,6 +15,14 @@ Route::group(['prefix'=>'admin'], function(){
     Route::group(['middleware' => 'admin.auth'], function(){
         Route::get('/dashboard',[HomeController::class, 'index'])->name('admin.dashboard');
         Route::get('/logout',[HomeController::class, 'logout'])->name('admin.logout');
+        //Category Routes
+        Route::get('/categories/index',[CategoryController::class, 'index'])->name('categories.index');
+        Route::get('/categories/create',[CategoryController::class, 'create'])->name('categories.create');
+        Route::post('/categories/store',[CategoryController::class, 'store'])->name('categories.store');
+
+        Route::get('/categories/edit',[CategoryController::class, 'edit'])->name('categories.edit');
+        Route::get('/categories/update',[CategoryController::class, 'update'])->name('categories.update');
+        Route::get('/categories/destroy',[CategoryController::class, 'destroy'])->name('categories.destroy');
     });
 });
     
