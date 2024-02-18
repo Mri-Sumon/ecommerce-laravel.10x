d74a3c50ea438e2586f6cef66241efb700a74111
mrisumon <mrisumon121@gmail.com>
2024-02-19 00:34:56 +0600

    create subcategory

--- app/Http/Controllers/admin/SubCategoryController.php
+++ app/Http/Controllers/admin/SubCategoryController.php
@@ -2,81 +2,63 @@
 namespace App\Http\Controllers\admin;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
-use App\Models\TempImage;
+use App\Models\SubCategory;
 use Illuminate\Http\Request;
+use App\Models\TempImage;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\File;
 use Intervention\Image\ImageManager;
 use Intervention\Image\Drivers\Gd\Driver;
 
-class CategoryController extends Controller
+class SubCategoryController extends Controller
 {
     public function index(Request $request)
     {
-        $categories = Category::latest('id');
+        $subCategories = SubCategory::latest('id')
+            ->leftJoin('categories', 'categories.id', '=', 'sub_categories.category_id')
+            ->select('sub_categories.*', 'categories.name as categoryName');
     
         if (!empty($request->get('keyword'))) {
-            $categories->where('name', 'like', '%' . $request->get('keyword') . '%');
+            $subCategories->where('sub_categories.name', 'like', '%' . $request->get('keyword') . '%');
+            $subCategories->orWhere('categories.name', 'like', '%' . $request->get('keyword') . '%');
         }
     
-        $categories = $categories->paginate(10);
-    
-        return view('admin.category.list', compact('categories'));
+        $subCategories = $subCategories->paginate(10);
+        return view('admin.sub_category.list', compact('subCategories'));
     }
     
 
     public function create(){
-        return view('admin.category.create');
+        $categories = Category::orderBy('id', 'asc')->get();
+        $data['categories']=$categories;
+        return view('admin.sub_category.create', $data);
     }
 
     public function store(Request $request){
         $validator = Validator::make($request->all(),[
             'name' => 'required', 
-            'slug' => 'required|unique:categories', 
+            'slug' => 'required|unique:sub_categories',
+            'category_id' => 'required',
+            'status' => 'required',
         ]);
 
         if($validator->passes()){
             
             $createBy = Auth::user()->id;
 
-            $category = new Category();
-            // $category->image = $request->image;
-            $category->name = $request->name;
-            $category->slug = $request->slug;
-            $category->status = $request->status;
-            $category->created_by = $createBy;
-            $category->save();
+            $subCategory = new SubCategory();
+            $subCategory->category_id = $request->category_id;
+            $subCategory->name = $request->name;
+            $subCategory->slug = $request->slug;
+            $subCategory->status = $request->status;
+            $subCategory->created_by = $createBy;
+            $subCategory->save();
 
-            // save image here 
-            if(!empty($request->image_id)){
-                $tempImage = TempImage::find($request->image_id);
-                $extArray = explode('.',$tempImage->name);
-                $ext = last($extArray);
-                $newImageName = $category->id.'.'.$ext;
-                $sPath = public_path().'/temp/'.$tempImage->name;
-                $dPath = public_path().'/uploads/category/'.$newImageName;
-                File::copy($sPath, $dPath);
-                $category->image = $newImageName;
-                $category->save();
-
-                $dPath=public_path().'/uploads/category/thumb/'.$newImageName;
-                if($sPath){
-                    $manager = new ImageManager(new Driver());
-                    $img = $manager->read($sPath);
-                    // $img = $img->resize(450, 600);
-                    // image resize ratio wise
-                    $img->resize(450, 600, function ($constraint) {
-                        $constraint->aspectRatio();
-                    });
-                    $img->save($dPath);
-                }
-            }
-
-            $request->session()->flash('success', 'Category added successfully!');
+            $request->session()->flash('success', 'Sub Category creaded successfully!');
             return response()->json([
                 'status' => true, 
-                'message' => 'Category added successfully'
+                'message' => 'Sub Category created successfully'
             ]);
 
         }else{
@@ -93,7 +75,7 @@ class CategoryController extends Controller
         if(empty($category)){
             return redirect()->route('categories.index');
         }
-        return view('admin.category.edit',compact('category'));
+        return view('admin.sub_category.edit',compact('category'));
     }
 
     public function update($categoryId, Request $request){
@@ -113,7 +95,6 @@ class CategoryController extends Controller
         
         $validator = Validator::make($request->all(),[
             'name' => 'required',
-            //$category->id এই আইডির রো-তে যদি, অলরেডি slug থাকে, তাহলে unique কীনা তা চেক করবে না।
             'slug' => 'required|unique:categories,slug,'.$category->id.',id',
         ]);
 
@@ -148,7 +129,6 @@ class CategoryController extends Controller
                 if($sPath){
                     $manager = new ImageManager(new Driver());
                     $img = $manager->read($sPath);
-                    // $img = $img->resize(450, 600);
                     // image resize ratio wise
                     $img->resize(450, 600, function ($constraint) {
                         $constraint->aspectRatio();
--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AdminLoginController;
 use App\Http\Controllers\admin\CategoryController;
+use App\Http\Controllers\admin\SubCategoryController;
 use App\Http\Controllers\admin\HomeController;
 use App\Http\Controllers\admin\TempImagesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
 Route::get('/', function () {return view('welcome');});
-
 Route::group(['prefix'=>'admin'], function(){
     Route::group(['middleware' => 'admin.guest'], function(){
         Route::get('/login',[AdminLoginController::class, 'index'])->name('admin.login');
@@ -18,10 +18,24 @@ Route::group(['prefix'=>'admin'], function(){
     Route::group(['middleware' => 'admin.auth'], function(){
         Route::get('/dashboard',[HomeController::class, 'index'])->name('admin.dashboard');
         Route::get('/logout',[HomeController::class, 'logout'])->name('admin.logout');
-        //Category Routes
+
+        //CATEGORY ROUTES
         Route::get('/categories',[CategoryController::class, 'index'])->name('categories.index');
         Route::get('/categories/create',[CategoryController::class, 'create'])->name('categories.create');
         Route::post('/categories/store',[CategoryController::class, 'store'])->name('categories.store');
+        Route::get('/categories/{categoryId}/edit',[CategoryController::class, 'edit'])->name('categories.edit');
+        Route::put('/categories/{categoryId}',[CategoryController::class, 'update'])->name('categories.update');
+        Route::delete('/categories/{categoryId}',[CategoryController::class, 'destroy'])->name('categories.delete');
+
+        //SUBCATEGORY ROUTES
+        Route::get('/sub-categories',[SubCategoryController::class, 'index'])->name('sub-categories.index');
+        Route::get('/sub-categories/create',[SubCategoryController::class, 'create'])->name('sub-categories.create');
+        Route::post('/sub-categories/store',[SubCategoryController::class, 'store'])->name('sub-categories.store');
+
+        //TEMP IMAGE CREATE
+        Route::post('/upload-temp-image',[TempImagesController::class, 'create'])->name('temp-images.create');
+
+        //CREATE SLUG
         Route::get('/getSlug', function(Request $request){
             $slug = '';
             if(!empty($request->title)){
@@ -33,10 +47,23 @@ Route::group(['prefix'=>'admin'], function(){
                 'slug' => $slug
             ]);
         })->name('getSlug');
-        Route::post('/upload-temp-image',[TempImagesController::class, 'create'])->name('temp-images.create');
-        Route::get('/categories/{categoryId}/edit',[CategoryController::class, 'edit'])->name('categories.edit');
-        Route::put('/categories/{categoryId}',[CategoryController::class, 'update'])->name('categories.update');
-        Route::delete('/categories/{categoryId}',[CategoryController::class, 'destroy'])->name('categories.delete');
+
+
+
+
+        
+        
+        
+
+
+
+
+
+
+
+
+
+
     });
 });
     
