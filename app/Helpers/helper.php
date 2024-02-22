d989852e810fc7e020e1bff3c0aa2abd9326c6fb
mrisumon <mrisumon121@gmail.com>
2024-02-22 17:04:01 +0600

    Home page dinamic done

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -1,12 +1,16 @@
 <?php
     use App\Models\Category;
 
-
-    function getCategories(){
-        return Category::orderBy('sort','ASC')->get();
-    }
     
-
+    function getCategories(){
+        return Category::orderBy('sort', 'ASC')
+        ->with(['sub_category' => function ($query) {
+            $query->orderBy('sort', 'ASC'); 
+        }])
+        ->where('show_on_home', 'Yes')
+        ->where('status', 1)
+        ->get();
+    }
 
 
 ?>
--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -1,11 +1,21 @@
 <?php
-
 namespace App\Http\Controllers;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class FrontController extends Controller
 {
     public function index(){
-        return view("front.home");  
+
+        $featuredProduct = Product::orderBy('sort', 'ASC')->where('is_featured','Yes')->where('status', 1)->take(8)->get();
+        $topSellingProduct = Product::orderBy('sort', 'ASC')->where('is_top_selling','Yes')->where('status', 1)->take(8)->get();
+        $latestProduct = Product::orderBy('id', 'DESC')->where('status', 1)->take(8)->get();
+
+        $data['featuredProduct']=$featuredProduct;
+        $data['topSellingProduct']=$topSellingProduct;
+        $data['latestProduct']=$latestProduct;
+
+        return view("front.home", $data);
+
     }
 }
