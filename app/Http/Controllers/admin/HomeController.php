1a49933dabb393de7f1e587392efeda9a68a63fb
mrisumon <mrisumon121@gmail.com>
2024-03-14 02:20:28 +0600

    Admin dashboard done

--- app/Http/Controllers/admin/HomeController.php
+++ app/Http/Controllers/admin/HomeController.php
@@ -2,7 +2,9 @@
 namespace App\Http\Controllers\admin;
 use App\Http\Controllers\Controller;
 use App\Models\Order;
-use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
@@ -10,8 +12,65 @@ class HomeController extends Controller
     public function index(){
 
         $totalOrders = Order::where('status','!=','canceled' )->count();
+        $totalProducts = Product::count();
+        $totalCustomers = User::where('role',1)->count();
 
-        return view('admin.dashboard');
+        //Overall revenue:
+        $totalRevenue = Order::where('status','!=','canceled' )->sum('grant_total');
+
+
+
+        //This month revenue:
+        //Get current month start date.
+        $startOfMonth = Carbon::now()->startOfMonth()->format('Y-m-d');
+        //Get current date.
+        $currentDate = Carbon::now()->format('Y-m-d');
+
+        $revenueThisMonth = Order::where('status','!=','canceled')
+                            ->whereDate('created_at','>=',$startOfMonth)
+                            ->whereDate('created_at','<=',$currentDate)
+                            ->sum('grant_total');
+
+
+
+        //Last month revenue:
+        //Get last month start date.
+        $lastMothStartDate = Carbon::now()->subMonth()->startOfMonth()->format('Y-m-d');
+        //Get last month end date.
+        $lastMothEndDate = Carbon::now()->subMonth()->endOfMonth()->format('Y-m-d');
+        //Get last month name.
+        $lastMothName = Carbon::now()->subMonth()->startOfMonth()->format('M');
+
+        $revenueLastMonth = Order::where('status','!=','canceled')
+                            ->whereDate('created_at','>=',$lastMothStartDate)
+                            ->whereDate('created_at','<=',$lastMothEndDate)
+                            ->sum('grant_total');
+
+
+
+        //Last 30 Days revenue:
+        //Get first date of last 30 Days date.
+        $lastThirtyStartDate = Carbon::now()->subDays(30)->format('Y-m-d');
+
+        $revenueLastThirtyDays = Order::where('status','!=','canceled')
+                            ->whereDate('created_at','>=',$lastThirtyStartDate)
+                            ->whereDate('created_at','<=',$currentDate)
+                            ->sum('grant_total');
+
+
+
+        return view('admin.dashboard',[
+            'totalOrders' => $totalOrders,
+            'totalProducts' => $totalProducts,
+            'totalCustomers' => $totalCustomers,
+            'totalRevenue' => $totalRevenue,
+            'revenueThisMonth' => $revenueThisMonth,
+            'revenueLastMonth' => $revenueLastMonth,
+            'lastMothName' => $lastMothName,
+            'revenueLastThirtyDays' => $revenueLastThirtyDays,
+        ]);
+
+        
     }
 
     public function logout(){
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,6 +1,6 @@
 @extends('admin.layouts.app')
 @section('content')
-	<!-- Content Header (Page header) -->
+
 	<section class="content-header">					
 		<div class="container-fluid">
 			<div class="row mb-2">
@@ -12,43 +12,56 @@
 				</div>
 			</div>
 		</div>
-		<!-- /.container-fluid -->
 	</section>
-	<!-- Main content -->
+
+
 	<section class="content">
-		<!-- Default box -->
 		<div class="container-fluid">
 			<div class="row">
+
 				<div class="col-lg-4 col-6">							
 					<div class="small-box card">
 						<div class="inner">
-							<h3>150</h3>
+							<h3>{{$totalOrders}} Nos</h3>
 							<p>Total Orders</p>
 						</div>
 						<div class="icon">
 							<i class="ion ion-bag"></i>
 						</div>
-						<a href="#" class="small-box-footer text-dark">More info <i class="fas fa-arrow-circle-right"></i></a>
+						<a href="{{route('orders.index')}}" class="small-box-footer text-dark">More info <i class="fas fa-arrow-circle-right"></i></a>
 					</div>
 				</div>
 				
 				<div class="col-lg-4 col-6">							
 					<div class="small-box card">
 						<div class="inner">
-							<h3>50</h3>
+							<h3>{{$totalProducts}} Nos</h3>
+							<p>Total Products</p>
+						</div>
+						<div class="icon">
+							<i class="ion ion-bag"></i>
+						</div>
+						<a href="{{route('products.index')}}" class="small-box-footer text-dark">More info <i class="fas fa-arrow-circle-right"></i></a>
+					</div>
+				</div>
+				
+				<div class="col-lg-4 col-6">							
+					<div class="small-box card">
+						<div class="inner">
+							<h3>{{$totalCustomers}} Nos</h3>
 							<p>Total Customers</p>
 						</div>
 						<div class="icon">
 							<i class="ion ion-stats-bars"></i>
 						</div>
-						<a href="#" class="small-box-footer text-dark">More info <i class="fas fa-arrow-circle-right"></i></a>
+						<a href="{{route('users.index')}}" class="small-box-footer text-dark">More info <i class="fas fa-arrow-circle-right"></i></a>
 					</div>
 				</div>
 				
 				<div class="col-lg-4 col-6">							
 					<div class="small-box card">
 						<div class="inner">
-							<h3>$1000</h3>
+							<h3>${{number_format($totalRevenue,2)}}</h3>
 							<p>Total Sale</p>
 						</div>
 						<div class="icon">
@@ -57,11 +70,52 @@
 						<a href="javascript:void(0);" class="small-box-footer">&nbsp;</a>
 					</div>
 				</div>
+				
+				<div class="col-lg-4 col-6">							
+					<div class="small-box card">
+						<div class="inner">
+							<h3>${{number_format($revenueThisMonth,2)}}</h3>
+							<p>This Month Total Sale</p>
+						</div>
+						<div class="icon">
+							<i class="ion ion-person-add"></i>
+						</div>
+						<a href="javascript:void(0);" class="small-box-footer">&nbsp;</a>
+					</div>
+				</div>
+				
+				
+				<div class="col-lg-4 col-6">							
+					<div class="small-box card">
+						<div class="inner">
+							<h3>${{number_format($revenueLastMonth,2)}}</h3>
+							<p>Last Month Total Sale ({{ $lastMothName }})</p>
+						</div>
+						<div class="icon">
+							<i class="ion ion-person-add"></i>
+						</div>
+						<a href="javascript:void(0);" class="small-box-footer">&nbsp;</a>
+					</div>
+				</div>
+				
+				
+				<div class="col-lg-4 col-6">							
+					<div class="small-box card">
+						<div class="inner">
+							<h3>${{number_format($revenueLastThirtyDays,2)}}</h3>
+							<p>Last 30 Days Total Sale</p>
+						</div>
+						<div class="icon">
+							<i class="ion ion-person-add"></i>
+						</div>
+						<a href="javascript:void(0);" class="small-box-footer">&nbsp;</a>
+					</div>
+				</div>
+				
+
 			</div>
 		</div>					
-		<!-- /.card -->
 	</section>
-	<!-- /.content -->
 @endsection
 
 
